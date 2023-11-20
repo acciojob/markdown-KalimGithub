9721e6bf007918e879a8c2b0fa225717df7b853e
KalimGithub <sk22kalim@gmail.com>
2023-11-20 21:01:31 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,24 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+{/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */}
 
+import React, {useState} from 'react'
+import ReactMarkdown from 'react-markdown'
+
+const App = () => {
+    const [text, setText] = useState('');
+  return (
+    <div className='app'>
+          <textarea name='text'
+              id='text'
+              className='textarea'
+              value={text}
+              onChange={(e) =>
+                  setText(e.target.value)}>
+          </textarea>
+        <div className='preview'>
+            <ReactMarkdown>{text}</ReactMarkdown>
+        </div>
+    </div>
+  )
+}
+
+export default App
